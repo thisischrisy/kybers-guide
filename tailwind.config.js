57cd3febe84ce96027641ef734cdf42c60891f84
thisischrisy <christopher.s1.yim@gmail.com>
2025-08-12 23:14:34 +0900

    commit & push

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
           gold: "#CFAE5B",
           ink: "#e8f2f2",
           card: "#0B2B2B",
-          line: "#134444"
+          line: "#134444" 
         }
       },
       boxShadow: { card: "0 1px 0 rgba(255,255,255,0.05), 0 8px 24px rgba(0,0,0,0.35)" },
